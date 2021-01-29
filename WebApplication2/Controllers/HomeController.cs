8846a4539ed25b733ee2c5816ddebbce36d02d77
штыхлин евгений <jeka_shtykhlin@mail.ru>
2021-01-29 19:29:12 +0600

    del str

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -18,7 +18,6 @@ namespace WebApplication2.Controllers
             ViewBag.Message = "Your application description page.";
             var color = ConsoleColor.Red;
             Console.BackgroundColor= color;
-            string s=String.Empty;
             return View();
         }
 
